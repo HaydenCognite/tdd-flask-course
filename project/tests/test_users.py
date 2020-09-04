03e9c502256dbb944fee55a2c5471b2bca48a51b
Hayden Reece Hohns <hayden.hohns@cognite.com>
2020-09-04 10:06:47 +0200

    New tests added.

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -21,7 +21,11 @@ def test_add_user(test_app, test_database):
 
 def test_add_user_invalid_json(test_app, test_database):
     client = test_app.test_client()
-    resp = client.post("/users", data=json.dumps({}), content_type="application/json",)
+    resp = client.post(
+        "/users",
+        data=json.dumps({}),
+        content_type="application/json",
+    )
     data = json.loads(resp.data.decode())
     assert resp.status_code == 400
     assert "Input payload validation failed" in data["message"]
@@ -29,7 +33,11 @@ def test_add_user_invalid_json(test_app, test_database):
 
 def test_add_user_invalid_json_keys(test_app, test_database):
     client = test_app.test_client()
-    resp = client.post("/users", data=json.dumps({}), content_type="application/json",)
+    resp = client.post(
+        "/users",
+        data=json.dumps({}),
+        content_type="application/json",
+    )
     data = json.loads(resp.data.decode())
     assert resp.status_code == 400
     assert "Input payload validation failed" in data["message"]
@@ -80,6 +88,32 @@ def test_all_users(test_app, test_database, add_user):
 def test_single_user_incorrect_id(test_app, test_database):
     client = test_app.test_client()
     resp = client.get("/users/999")
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert "User 999 does not exist" in data["message"]
+
+
+def test_remove_user(test_app, test_database, add_user):
+    test_database.session.query(User).delete()
+    user = add_user("user-to-be-removed", "remove-me@testdriven.io")
+    client = test_app.test_client()
+    resp_one = client.get("/users")
+    data = json.loads(resp_one.data.decode())
+    assert resp_one.status_code == 200
+    assert len(data) == 1
+    resp_two = client.delete(f"/users/{user.id}")
+    data = json.loads(resp_two.data.decode())
+    assert resp_two.status_code == 200
+    assert "remove-me@testdriven.io was removed!" in data["message"]
+    resp_three = client.get("/users")
+    data = json.loads(resp_three.data.decode())
+    assert resp_three.status_code == 200
+    assert len(data) == 0
+
+
+def test_remove_user_incorrect_id(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.delete("/users/999")
     data = json.loads(resp.data.decode())
     assert resp.status_code == 404
     assert "User 999 does not exist" in data["message"]
