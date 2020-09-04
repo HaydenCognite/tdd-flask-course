4b11404567f3058e5f9ab9a680f05113b9ee0a9b
Hayden Reece Hohns <hayden.hohns@cognite.com>
2020-09-04 10:32:32 +0200

    Updated tests and user methods for more functionality.

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -3,6 +3,8 @@
 
 import json
 
+import pytest
+
 # from project import db
 from project.api.models import User
 
@@ -21,11 +23,7 @@ def test_add_user(test_app, test_database):
 
 def test_add_user_invalid_json(test_app, test_database):
     client = test_app.test_client()
-    resp = client.post(
-        "/users",
-        data=json.dumps({}),
-        content_type="application/json",
-    )
+    resp = client.post("/users", data=json.dumps({}), content_type="application/json",)
     data = json.loads(resp.data.decode())
     assert resp.status_code == 400
     assert "Input payload validation failed" in data["message"]
@@ -33,11 +31,7 @@ def test_add_user_invalid_json(test_app, test_database):
 
 def test_add_user_invalid_json_keys(test_app, test_database):
     client = test_app.test_client()
-    resp = client.post(
-        "/users",
-        data=json.dumps({}),
-        content_type="application/json",
-    )
+    resp = client.post("/users", data=json.dumps({}), content_type="application/json",)
     data = json.loads(resp.data.decode())
     assert resp.status_code == 400
     assert "Input payload validation failed" in data["message"]
@@ -117,3 +111,46 @@ def test_remove_user_incorrect_id(test_app, test_database):
     data = json.loads(resp.data.decode())
     assert resp.status_code == 404
     assert "User 999 does not exist" in data["message"]
+
+
+def test_update_user(test_app, test_database, add_user):
+    user = add_user("user-to-be-updated", "update-me@testdriven.io")
+    client = test_app.test_client()
+    resp_one = client.put(
+        f"/users/{user.id}",
+        data=json.dumps({"username": "me", "email": "me@testdriven.io"}),
+        content_type="application/json",
+    )
+    data = json.loads(resp_one.data.decode())
+    assert resp_one.status_code == 200
+    assert f"{user.id} was updated!" in data["message"]
+    resp_two = client.get(f"/users/{user.id}")
+    data = json.loads(resp_two.data.decode())
+    assert resp_two.status_code == 200
+    assert "me" in data["username"]
+    assert "me@testdriven.io" in data["email"]
+
+
+@pytest.mark.parametrize(
+    "user_id, payload, status_code, message",
+    [
+        [1, {}, 400, "Input payload validation failed"],
+        [1, {"email": "me@testdriven.io"}, 400, "Input payload validation failed"],
+        [
+            999,
+            {"username": "me", "email": "me@testdriven.io"},
+            404,
+            "User 999 does not exist",
+        ],
+    ],
+)
+def test_update_user_invalid(
+    test_app, test_database, user_id, payload, status_code, message
+):
+    client = test_app.test_client()
+    resp = client.put(
+        f"/users/{user_id}", data=json.dumps(payload), content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == status_code
+    assert message in data["message"]
